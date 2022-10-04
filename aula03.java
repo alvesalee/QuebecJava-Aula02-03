c1f49c28b71141933b70f94a8c8f948ceb22069c
ALEXANDRE ALVES SILVA <105252517+alvesalee@users.noreply.github.com>
2022-10-03 22:01:10 -0300

    Add files via upload

---
+++ aula03.java
@@ -0,0 +1,57 @@
+package QuebecJava;
+
+public class aula03 {
+
+	public static void main (String[] args) {
+		
+		byte b1 = 10;
+		byte b2 = 20;
+		
+		short s1 = 20000;
+		//short s2 = 40000;
+		
+		//int i1 = -100000000;
+		int i2 = 28500;
+		
+		long l1 = 100000000000000L;
+		long l2 = 200400500600500L;
+		
+		//float f1 = 4.5;
+		float f2 = 10.68f;
+		
+		double d1 = 85.69;
+		double d2 = 99.84d;
+		
+		char c1 = 'W';
+		//char c2 = 'Tw';
+		char c3 ='\u0057'; 
+		
+		String st1 = "Alexandre";
+		String st2 = "Isabela";
+		String st3 = "dannklvndavnaonv 14398t3190&&*¨%";
+	
+		String dt1 = "16/04/1999";
+				
+		boolean bo1 = true;
+		boolean bo2 = false; 
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(s1);
+		System.out.println(i2);
+		System.out.println(l1);
+		System.out.println(l2);
+		System.out.println(f2);
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(c1);
+		System.out.println(c3);
+		System.out.println(st1);
+		System.out.println(st2);
+		System.out.println(st3);
+		System.out.println(dt1);
+		System.out.println(bo1);
+		System.out.println(bo2);
+		
+	}
+}
